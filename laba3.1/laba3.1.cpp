a88c5573cae8784badbbf51c2931b1949d3bdd35
popofffnik <57560494+popofffnik@users.noreply.github.com>
2022-12-23 18:26:02 +0300

    Добавьте файлы проекта.

---
+++ laba3.1/laba3.1.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+
+#include <Windows.h>
+
+using namespace std;
+
+
+
+int main()
+
+{
+
+    int a = 35;
+
+    int* pointer_a = &a;
+
+    cout << "pointer_a = " << pointer_a << endl;
+
+    cout << "*pointer_a = " << *pointer_a << endl;
+
+    *pointer_a = 45;
+
+    cout << "a = " << a << endl;
+
+
+
+    cout << "\n-------\n";
+
+
+
+    int array[] = { 3,6,9,12 };
+
+    cout << "array[]" << endl;
+
+    for (int* p = &array[0]; p <= &array[3]; p++)
+
+    {
+
+        cout << *p << " ";
+
+    }
+
+
+
+    cout << "\n-------\n";
+
+
+
+    int b = 5;
+
+    int* const pointer_b = &b;
+
+    cout << "pointer_b = " << pointer_b << endl;
+
+    cout << "*pointer_b = " << *pointer_b << endl;
+
+    //pointer_b = &a; <ошибка>
+
+    *pointer_b = 15;
+
+    cout << "*pointer_b = " << *pointer_b << endl;
+
+}
